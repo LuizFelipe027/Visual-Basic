c8a0d3a7c49c77828de99bc957a8069c0875dfbb
Luiz Felipe Ribeiro Paixão <66446100+LuizFelipe027@users.noreply.github.com>
2021-11-30 16:34:38 -0300

    Add files via upload

---
+++ 15. Operadores de Comparacao/15. Operadores de Comparacao/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim txtNome As String = "Luiz"
+
+        'Declara a variável que armazeranará o nome inserido no textbox'
+        Dim strNome As String
+        strNome = txtBox.Text
+
+        'O nome é João?'
+        If strNome <> "João" Then
+            MessageBox.Show("O nome NÃO é João", "Comparação")
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'Declara a variavel'
+        Dim intNumero As Integer
+        Try
+            'Pega o número do textbox'
+            intNumero = TextBox1.Text
+        Catch
+            Exit Sub
+        End Try
+        'O número é menor que 27?'
+        If intNumero < 27 Then
+            MessageBox.Show("intNumero é menor que 27? SIM!", "Comparação")
+        Else
+            MessageBox.Show("intNumero é menor que 27? NÂO!", "Comparação")
+        End If
+
+
+    End Sub
+End Class
---
+++ 15. Operadores de Comparacao/15. Operadores de Comparacao/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("15. Operadores de Comparacao")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("15. Operadores de Comparacao")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("16f1fc4b-b218-4421-860c-8cc50746c82d")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
