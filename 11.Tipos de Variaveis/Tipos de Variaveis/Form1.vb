159a1f307af2a8bf1743e7d2903162d675e7a2dd
Luiz Felipe Ribeiro Paixão <66446100+LuizFelipe027@users.noreply.github.com>
2021-11-30 14:21:22 -0300

    Add files via upload

---
+++ 11.Tipos de Variaveis/Tipos de Variaveis/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim intIdade As Integer = 20
+        Dim dblAltura As Double = 1.9
+        Dim strNome As String = "Luiz"
+        Dim blnPossuiFilhos As Boolean = False
+
+        MsgBox(intIdade)
+        MsgBox(dblAltura)
+        MsgBox(strNome)
+        MsgBox(blnPossuiFilhos)
+    End Sub
+End Class
