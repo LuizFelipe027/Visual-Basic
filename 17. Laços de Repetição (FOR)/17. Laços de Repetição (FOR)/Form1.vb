cd8387aea762bf6d75f5cffb2c442dbe1d2a91d5
Luiz Felipe Ribeiro Paixão <66446100+LuizFelipe027@users.noreply.github.com>
2021-11-30 16:53:29 -0300

    Add files via upload

---
+++ 17. Laços de Repetição (FOR)/17. Laços de Repetição (FOR)/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Declara Variável'
+        Dim intConta As Integer
+
+        'Chamada da Sub Rotina LimpaLista'
+        LimpaLista()
+
+        'Loop FOR para preencher lista'
+        For intConta = 1 To 5
+            'Adiciona item na lista'
+            lstDados.Items.Add("Eu sou o item: " & intConta.ToString & " na lista !")
+        Next
+
+    End Sub
+
+    'Criando uma Sub Rotina:'
+    Private Sub LimpaLista()
+        lstDados.Items.Clear()
+    End Sub
+
+End Class
