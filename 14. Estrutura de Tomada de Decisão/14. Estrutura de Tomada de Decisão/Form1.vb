d518733da900d8e67fa68ab1b19a1a1bb7ec5763
Luiz Felipe Ribeiro Paixão <66446100+LuizFelipe027@users.noreply.github.com>
2021-11-30 15:57:36 -0300

    Add files via upload

---
+++ 14. Estrutura de Tomada de Decisão/14. Estrutura de Tomada de Decisão/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim intNumero As Integer = 27
+
+        'Tomada de decisao'
+        If intNumero = 1000 Then
+            MessageBox.Show("IntNumero é exatamente 1000!", "IF Simples")
+        ElseIf intNumero = 27 Then
+            MessageBox.Show("IntNumero é exatamente 27!", "IF Simples")
+        Else
+            MessageBox.Show("IntNumero não é exatamente 1000 e 27!", "IF Simples")
+        End If
+    End Sub
+End Class
