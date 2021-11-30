656cfc26af29b3573d5f146e820ba3e983d7ae01
Luiz Felipe Ribeiro Paixão <66446100+LuizFelipe027@users.noreply.github.com>
2021-11-30 13:58:52 -0300

    Add files via upload

---
+++ VariaveisOk/VariaveisOk/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim intNumero As Integer
+        intNumero = 27
+        intNumero = intNumero + 1
+        MessageBox.Show("intNumero + 1 = " & intNumero.ToString, "Variáveis")
+
+    End Sub
+End Class
