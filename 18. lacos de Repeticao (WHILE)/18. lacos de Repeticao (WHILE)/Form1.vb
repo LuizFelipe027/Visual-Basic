141e2408929aa0eacccbc82d086a3fcbc13f2dbd
Luiz Felipe Ribeiro Paixão <66446100+LuizFelipe027@users.noreply.github.com>
2021-12-01 13:27:24 -0300

    Add files via upload

---
+++ 18. lacos de Repeticao (WHILE)/18. lacos de Repeticao (WHILE)/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Declara as variaveis'
+        Dim intConta As Integer = 0
+
+        'Chamada da sub Rotina LimpaLista'
+        LimpaLista()
+
+        'Executa loop enquanto intConta < 5
+        Do While intConta < 5
+            'Adiciona o número na lista
+            TextBox2.Items.Add(intConta.ToString)
+            intConta += 1
+        Loop
+    End Sub
+
+    Private Sub LimpaLista()
+        'limpa a lista'
+        TextBox2.Items.clear()
+
+    End Sub
+
+End Class
