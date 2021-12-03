d3302cc69c8e2ed8085c7ed8457fcaf7d7aa49a3
Luiz Felipe Ribeiro Paixão <66446100+LuizFelipe027@users.noreply.github.com>
2021-12-03 14:14:20 -0300

    Add files via upload

---
+++ 20-29. VizualizadorImagens_Trabalhando+com+Radio+Buttons/VizualizadorImagens_Trabalhando com Radio Buttons/VizualizadorImagens/frmPrincipal.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmPrincipal
+  Inherits System.Windows.Forms.Form
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmPrincipal))
+        Me.btnSobre = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnSobre
+        '
+        Me.btnSobre.Location = New System.Drawing.Point(197, 12)
+        Me.btnSobre.Name = "btnSobre"
+        Me.btnSobre.Size = New System.Drawing.Size(75, 23)
+        Me.btnSobre.TabIndex = 0
+        Me.btnSobre.Text = "Sobre"
+        Me.btnSobre.UseVisualStyleBackColor = True
+        '
+        'frmPrincipal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnSobre)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmPrincipal"
+        Me.Text = "Vizualizador de Imagens"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnSobre As Button
+End Class
---
+++ 20-29. VizualizadorImagens_Trabalhando+com+Radio+Buttons/VizualizadorImagens_Trabalhando com Radio Buttons/VizualizadorImagens/frmPrincipal.vb
@@ -0,0 +1,6 @@
+﻿Public Class frmPrincipal
+    Private Sub btnSobre_Click(sender As Object, e As EventArgs) Handles btnSobre.Click
+        frmSobre.ShowDialog()
+
+    End Sub
+End Class
